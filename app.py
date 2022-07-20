1fc68166150ad57f4955bf7dfaaf8ccca3ef160f
chema-hg <honrajm@yahoo.es>
2022-07-20 12:27:49 +0200

    En la función principal del programa creamos la variable clock para regular el frame rate del programa y que no vaya ni demasiado rapido ni demasiado lento. Luego con clock.tick(60) establecemos un frame rate maximo de 60 imagenes por segundo. La idea es que se vea un movimiento natural independientemente del procesador con el que se ejecute el programa.
    Luego pasamos a dibujar el primer objeto en pantalla que no es otra cosa que
    rellenar el fondo de la ventana con el color blanco. Para ello usamos el método .fill() con el objeto WIN ya creado anteriormente. Se le pasa el color que se quierea como argumento. Pero esto no dibuja inmediatamente el
    objeto creado en la pantalla. Para ello despues de poner los objetos hay que
    actualizar la pantalla. Se usa el comando pygame.display.update()

--- app.py
+++ app.py
@@ -9,16 +9,32 @@ pygame.init()
 WIDTH, HEIGHT = 800, 800
 # Se dibuja el area donde se realizara la simulacion
 # En donde se dibujaran los planetas.
+# El objeto WIN nos servirá para dibujar objetos en la pantalla.
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 # Ponemos el nombre de la ventana.
 pygame.display.set_caption("Simulación del Sistema Solar")
+
+# Color de relleno del fondo de la pantalla
+WHITE = (255, 255, 255)
 
 # La simulación del sistema solar se realiza en un bucle infinito.
 # El único evento que se ejecutara será el movimiento de los planetas.
 def main():
     run = True
+    # Para determinar el frame rate del programa y que no vaya ni muy deprisa
+    # ni muy despacio establecemos la variable clock
+    clock = pygame.time.Clock()
     
     while run:
+        # número máximo de veces que se actualizará la pantalla por segundo (frame rate maximo)
+        # Lo hacemos para asegurarnos de que el programa no vaya demasiado rapido.
+        clock.tick(60)
+        # Rellena el fondo de la pantalla con el color que le pasemos como argumento
+        # No se mostrara mientras no se actualice la pantalla.
+        WIN.fill(WHITE)
+        # Actualiza la pantalla. Los objetos que hayamos puesto se mostrarán en la misma.
+        pygame.display.update()
+        
         for event in pygame.event.get():
             # El unico evento que nos interesa registrar es cuando se
             # pulse en la esquina superior derecha la "x" para salir del programa.
