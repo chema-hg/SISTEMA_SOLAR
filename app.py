1bc42a481ef0f95e5fc0cc6f12e1f5e483686880
chema-hg <honrajm@yahoo.es>
2022-07-20 13:26:41 +0200

    Creamos la clase Planet para incorporar todos los valores y costantes que necesitaremos para recrear los planetas, asi como constantes fisicas para calcular fuerzas y movimientos.

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+''' Simulador del sistema solar.
+
+Simulador lo más exacto posible usando pygame como biblioteca
+de partida. '''
+
 #importamos la libreria para los graficos 2d 
 import pygame
 # Importamos la libreria para los calculos matematicos
@@ -16,6 +21,51 @@ pygame.display.set_caption("Simulación del Sistema Solar")
 
 # Color de relleno del fondo de la pantalla
 WHITE = (255, 255, 255)
+
+
+# Para construir los planetas vamos a utilizar la clase Planet()
+class Planet:
+    ''' Definicion de los Planetas.
+
+    Argumentos:
+    
+    x, y - Posición de los planetas en la pantalla.
+    radius - radio del planeta.
+    color - color del planeta.
+    mass - masa del planeta.'''
+    
+    # Constantes planetarias para realizar los calculos UNIDADES ASTRONOMICAS
+    # UNIDAD ASTRONOMICA - Distancia media entre la tierra y el sol en METROS.
+    AU = 149_597_870_700
+    # CONSTANTE GRAVITACIONAL - Para calcular la fuerza de atración de los planetas. (N*m2)/Kg2
+    G = 6.67428e-11
+    # La escala sirve para ajustar las unidades astronomicas a nuestro grafico. No podemos dibujar
+    # las distancias espaciales en nuestra pantalla. Por ello utilizamos la escala en la que aproximadamente
+    # 100 pixeles equivale a una UNIDAD ASTRONOMICA.
+    SCALE = 250 / AU
+    # Cada vez que actualice los frames o la pantalla, cuanto tiempo ha pasado en la realidad
+    # Vamos a establecerlo en un dia terrestre pero en en SEGUNDOS.
+    TIMESTEP = 3600 * 24 
+    
+    def __init__(self, x, y, radius, color, mass):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.mass = mass
+        
+        # Lista vacia para realizar un seguimiento de todos los puntos que sigue el planeta
+        # para poder dibujar su orbita.
+        self.orbit = [] 
+        # Nos dice si el planeta es el sol o no.
+        self.sun = False
+        # Distancia hasta el sol.
+        self.distance_to_sun = 0
+        
+        # Velocidad en los ejes x e y
+        self.x_vel = 0
+        self.y_vel = 0
+
 
 # La simulación del sistema solar se realiza en un bucle infinito.
 # El único evento que se ejecutara será el movimiento de los planetas.
